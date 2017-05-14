09b2d9289b47a2524be4855041ea59dbe143e061
wjx <550481089@qq.com>
2017-05-14 15:37:13 +0800

    新增缓存当前用户功能

--- app/src/main/java/com/example/lenovo_g50_70/notepad/activity/LandActivity.java
+++ app/src/main/java/com/example/lenovo_g50_70/notepad/activity/LandActivity.java
@@ -5,15 +5,51 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.lenovo_g50_70.notepad.R;
+import com.example.lenovo_g50_70.notepad.model.UserInfo;
+
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.LogInListener;
 
 public class LandActivity extends AppCompatActivity {
     private Button regUser;
+    private Button landUser;
+    private EditText edtUserName;
+    private EditText edtUserPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land);
+        initView();
+    }
+
+    private void initView() {
+        edtUserName = (EditText) findViewById(R.id.edtUserName);
+        edtUserPass = (EditText) findViewById(R.id.edtUserPass);
+        landUser = (Button) findViewById(R.id.btnLand);
+        landUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =edtUserName.getText().toString();
+                String pass =edtUserPass.getText().toString();
+                BmobUser.loginByAccount(name,pass, new LogInListener<UserInfo>() {
+
+                    @Override
+                    public void done(UserInfo user, BmobException e) {
+                        if(user!=null){
+                            Toast.makeText(getApplicationContext(),"登陆成功",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(getApplicationContext(),"登陆失败",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
         regUser= (Button) findViewById(R.id.regUser);
         regUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +57,6 @@ public class LandActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
             }
         });
+
     }
 }
